f256623bd894e1c7bd39b7dd9ec4420370cf79ce
tamasedami <damien.tamasese@gmail.com>
2021-04-12 10:05:11 +1200

    Test push, plus initial setup

---
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/JSONLayout.java
@@ -0,0 +1,13 @@
+package nz.ac.wgtn.swen301.assignment2;
+
+/**
+ * Hello world!
+ *
+ */
+public class JSONLayout
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
